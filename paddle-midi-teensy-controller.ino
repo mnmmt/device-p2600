54988b50f6105f5a14df02dd1803a46820f91bc2
Chris McCormick <chris@mccormick.cx>
2016-12-20 21:34:08 +0800

    Used exponential smoothing and thredholding to clean up noisy pot.

--- paddle-midi-teensy-controller.ino
+++ paddle-midi-teensy-controller.ino
@@ -5,15 +5,10 @@
 // bin/contrib/arduino-1.6.12/hardware/teensy/avr/cores/usb_midi/usb_private.h
 
 // noise tolerance
-const int THRESHOLD = 3;
+const int THRESHOLD = 4;
 
 // The number of pots
 const int pots = 2;
-
-// look up each pot
-//int pot_lookup[pots] = {A0, A1, A2, A3, A4, A5};
-//int pot_lookup[pots] = {A0, A4, A5};
-// int pot_lookup[pots] = {A0, A4, A5};
 
 int ledPin = 11;
 int btnState = 1;
@@ -22,14 +17,16 @@ int pot_lookup[pots] = {A0, A1};
 
 // previous read value for thresholding
 int lastreading[pots];
+int weighted[pots];
 
-// Set midi channel to send to 1
+// Set midi channel to send to
 int channel = 2;
 
 void setup()
 {
-  for ( int p = 0; p < pots; p++ ) {
+  for (int p = 0; p < pots; p++) {
     lastreading[p] = 0;
+    weighted[p] = 0;
   }
 
   // Standard midi rate
@@ -49,44 +46,26 @@ void setup()
 void loop()
 {
   // Get reading for all potentiometers
-  //for (int i = 0; i < pots; i++ ) {
-
-   // Read from the sensor
-   int i = 0;
-   int r = analogRead(pot_lookup[i]);
-
-   // If reading is different from the previous
-   if (abs(r - lastreading[i]) >= THRESHOLD && r / 8 != lastreading[i] / 8){
-     // Send midi data
-     usbMIDI.sendControlChange(i, r / 8, channel);
-    // Set last array value to current value
-    lastreading[i] = r;
-   }
-  //}
-
-  i = 1;
-  r = analogRead(pot_lookup[i]);
-  
-   // If reading is different from the previous
-   if (abs(r - lastreading[i]) >= THRESHOLD && r / 8 != lastreading[i] / 8){
-     // Send midi data
-     if (r / 8 < 4 && lastreading[i] / 8 >= 4) {
-       //usbMIDI.sendControlChange(i, r / 8, channel);
-       btnState = !btnState;
-       usbMIDI.sendControlChange(1, btnState ? 127 : 0, channel);
-       digitalWrite(ledPin, btnState ? HIGH : LOW);
-     }
-    // Set last array value to current value
-    lastreading[i] = r;
-   }
-
-  //if (r / 8 < 5 && lastreading[i] / 8 >= 5) {
-  //  btnState = !btnState;
-  //  usbMIDI.sendControlChange(1, btnState ? 127 : 0, channel);
-  //  digitalWrite(ledPin, btnState ? HIGH : LOW);
-  //  lastreading[i] = two;
-  //}
-  
+  for (int i = 0; i < pots; i++ ) {
+    // http://electronics.stackexchange.com/a/64699
+    weighted[i] += (analogRead(pot_lookup[i]) - weighted[i]) / 8;
+    int midival = weighted[i] / 8;
+    int midiprev = lastreading[i] / 8;
+    if (abs(lastreading[i] - weighted[i]) > THRESHOLD && midival != midiprev) {
+      //usbMIDI.sendControlChange(2, 64, channel);
+      if (i == 0) {
+        usbMIDI.sendControlChange(i, midival, channel);
+      } else if (i == 1) {
+        if (midival < 4 && midiprev >= 4) {
+          //usbMIDI.sendControlChange(i, r / 8, channel);
+          btnState = !btnState;
+          usbMIDI.sendControlChange(i, btnState ? 127 : 0, channel);
+          digitalWrite(ledPin, btnState ? HIGH : LOW);
+        }
+      }
+      lastreading[i] = weighted[i];
+    }
+  }
   // Delay output
-  delay(50);
+  delay(10);
 }
